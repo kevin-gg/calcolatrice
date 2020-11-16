54ef70deb70e468ca7351f339d023d24cf950f1f
kevin trentini <trentinikevin6@gmail.com>
2020-11-16 13:10:33 +0100

    Add project files.

---
+++ Operazioni/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace calc_1._0
+{
+    public class Operazioni
+    {
+        public static string Scelta()
+
+        {
+            
+            Console.WriteLine("Per eseguire:\nsomma digita 1 \ndifferenza digita 2 \nmoltiplicazione digita 3 \ndivisione digita 4 ");
+            string scelta = Console.ReadLine();
+            return scelta;
+        }
+
+            public static double Numero()
+        {
+            Console.WriteLine("inserisci un numero");
+            double n1 = Convert.ToDouble(Console.ReadLine());
+            return n1;
+        }
+        public static double Somma(double n1, double n2)
+        {
+
+            return n1 + n2;
+        }
+        public static double Differenza(double n1, double n2)
+        {
+
+            return n1 - n2;
+
+        }
+        public static double Moltiplicazione(double n1, double n2)
+        {
+            return n1 * n2;
+
+        }
+        public static double Divisione(double n1, double n2)
+        {
+            return n1 / n2;
+
+        }
+
+    }
+}
---
+++ calc 1.0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace calc_1._0
+{
+    class Program
+    {
+        private static string scelta;
+
+        static void Main(string[] args)
+        {
+            double n1 = Operazioni.Numero();
+            double n2 = Operazioni.Numero();
+            string scelta = Operazioni.Scelta();
+            double risultato = 0;
+            switch (scelta)
+            {
+                case "1":
+
+
+                    risultato = Operazioni.Somma(n1, n2);
+                    break;
+                case "2":
+
+
+                    risultato = Operazioni.Differenza(n1, n2);
+                    break;
+                case "3":
+
+
+                    risultato = Operazioni.Moltiplicazione(n1, n2);
+                    break;
+                case "4":
+
+
+                    risultato = Operazioni.Divisione(n1, n2);
+                    break;
+
+            }
+            Console.WriteLine($"{risultato}");
+        }
+    }
+}
